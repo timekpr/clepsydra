a09684774c395f8bfaeb7c246ce8c4f6e9d593f7
Olli-Pekka Wallin <opwallin@gmail.com>
2014-01-09 00:08:17 +0200

    Unstable code

--- app/users.cpp
+++ app/users.cpp
@@ -8,9 +8,20 @@ Users::Users(QObject *parent) :
 
 void Users::getStandardUsers()
 {
-    QDBusConnection bus = QDBusConnection::sessionBus();
-    // org.freedesktop.Accounts
-    QDBusInterface dbus_iface("org.freedesktop.DBus", "/org/freedesktop/DBus",
-                              "org.freedesktop.DBus", bus);
-    qDebug() << dbus_iface.call("ListNames").arguments().at(0);
+//    // https://github.com/mauios/qtaccountsservice
+//    QDBusConnection bus = QDBusConnection::systemBus();
+//    // org.freedesktop.Accounts
+//    QDBusInterface dbus_iface("org.freedesktop.Accounts", "/org/freedesktop/Accounts",
+//                              "org.freedesktop.DBus.Properties", bus);
+//                              //"org.freedesktop.Accounts.User", bus);
+
+
+
+//    // Calls DBus method
+//    QDBusMessage result = dbus_iface.call( "GetAll",
+//                                          "(a{sv})",
+//                                          "Interfaces" );
+//    qDebug() << result;
+//    //qDebug() << dbus_iface.isValid();
+//    //qDebug() << dbus_iface.call("Get").arguments().append(QString("(a{sv})"));
 }
