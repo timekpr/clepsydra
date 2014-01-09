8cc1de5827398dd72c8819043266e7fa05396d7c
Olli-Pekka Wallin <opwallin@gmail.com>
2014-01-09 23:52:47 +0200

    Add code to get some Avahi version info from system.

--- app/users.cpp
+++ app/users.cpp
@@ -8,20 +8,21 @@ Users::Users(QObject *parent) :
 
 void Users::getStandardUsers()
 {
+
+//    QDBusConnection::sessionBus().connect("org.gnome.SessionManager", "/org/gnome/SessionManager/Presence", "org.gnome.SessionManager.Presence" ,"StatusChanged", this, SLOT(MySlot(uint)));
 //    // https://github.com/mauios/qtaccountsservice
-//    QDBusConnection bus = QDBusConnection::systemBus();
+    QDBusConnection bus = QDBusConnection::systemBus();
 //    // org.freedesktop.Accounts
-//    QDBusInterface dbus_iface("org.freedesktop.Accounts", "/org/freedesktop/Accounts",
-//                              "org.freedesktop.DBus.Properties", bus);
-//                              //"org.freedesktop.Accounts.User", bus);
+    QDBusInterface adbus_iface("org.freedesktop.Accounts", "/org/freedesktop/Accounts/User1000",
+       "org.freedesktop.Accounts.User", bus);
+    qDebug() << adbus_iface.call( "UserName").arguments().at(0);
+
+    QDBusInterface dbus_iface("org.freedesktop.Avahi", "/",
+       "org.freedesktop.Avahi.Server", bus);
 
 
-
-//    // Calls DBus method
-//    QDBusMessage result = dbus_iface.call( "GetAll",
-//                                          "(a{sv})",
-//                                          "Interfaces" );
-//    qDebug() << result;
-//    //qDebug() << dbus_iface.isValid();
-//    //qDebug() << dbus_iface.call("Get").arguments().append(QString("(a{sv})"));
+    QVariant api = dbus_iface.call( "GetAPIVersion").arguments().at(0);
+    qDebug () << "Avahi api version" << api.toInt();
+    QVariant hostName = dbus_iface.call( "GetHostName").arguments().at(0);
+    qDebug () << "Running on " << hostName.toString();
 }
