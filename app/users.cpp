728f973d0756cb97b3b2e467d74c28b4b4472988
Olli-Pekka Wallin <opwallin@gmail.com>
2014-01-11 17:00:59 +0200

    Accoutns ifno retrieved from system

--- app/users.cpp
+++ app/users.cpp
@@ -15,14 +15,17 @@ void Users::getStandardUsers()
 //    // org.freedesktop.Accounts
     QDBusInterface adbus_iface("org.freedesktop.Accounts", "/org/freedesktop/Accounts/User1000",
        "org.freedesktop.Accounts.User", bus);
-    qDebug() << adbus_iface.call( "UserName").arguments().at(0);
 
-    QDBusInterface dbus_iface("org.freedesktop.Avahi", "/",
-       "org.freedesktop.Avahi.Server", bus);
+    qDebug() << "username" <<  adbus_iface.property("UserName").toString();
+    qDebug() << "account type" <<  adbus_iface.property("AccountType").toInt();
+    qDebug() << "Shell" <<  adbus_iface.property("Shell").toString();
+
+//    QDBusInterface dbus_iface("org.freedesktop.Avahi", "/",
+//       "org.freedesktop.Avahi.Server", bus);
 
 
-    QVariant api = dbus_iface.call( "GetAPIVersion").arguments().at(0);
-    qDebug () << "Avahi api version" << api.toInt();
-    QVariant hostName = dbus_iface.call( "GetHostName").arguments().at(0);
-    qDebug () << "Running on " << hostName.toString();
+//    QVariant api = dbus_iface.call( "GetAPIVersion").arguments().at(0);
+//    qDebug () << "Avahi api version" << api.toInt();
+//    QVariant hostName = dbus_iface.call( "GetHostName").arguments().at(0);
+//    qDebug () << "Running on " << hostName.toString();
 }
