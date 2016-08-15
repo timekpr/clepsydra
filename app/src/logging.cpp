e3114c73b671c2f6a14912d937784108f2b737d6
Olli-Pekka Wallin <opwallin@gmail.com>
2016-08-16 00:00:37 +0300

    Add license

--- app/src/logging.cpp
+++ app/src/logging.cpp
@@ -1,3 +1,16 @@
+// Copyright (c) 2014 Olli-Pekka Wallin <opwallin@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <QDebug>
 #include <QFile>
 #include <QDir>
@@ -55,7 +68,6 @@ void Logging::logHexData (const QString& data, const QString& header)
 void Logging::logByteData (unsigned char *array, int len)
 {
     QString result;
-    uint dd = 10;
     int numInLine = 0;
     result.reserve(3 * len - 1);
     for(unsigned char* i = array, *end = array + len; i < end; ++i, ++numInLine) {
